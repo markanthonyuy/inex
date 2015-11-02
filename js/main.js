bab6b27c1e767a40f9a9dfd56cc5cbe17457888a
MarkUy <macmac.uy@gmail.com>
2015-11-02 18:38:52 +0800

    init flexslider

--- js/main.js
+++ js/main.js
@@ -1,26 +1,49 @@
 (function($) {
-	
-	console.log('Hooray!');
+  
+  console.log('Hooray!');
 
-	$('.main-slideshow-nav').on('click', function(e) {
-		var _this = $(this),
-			target = _this.data('target');
+  /*$('.main-slideshow-nav').on('click', function(e) {
+    var _this = $(this),
+      target = _this.data('target');
 
-		if(_this.hasClass('active')) return;
+    if(_this.hasClass('active')) return;
 
-		$('.main-slideshow-nav').removeClass('active');
-		_this.addClass('active');
+    $('.main-slideshow-nav').removeClass('active');
+    _this.addClass('active');
 
-		$('.main-slideshow-content').removeClass('active');
-		$($('.main-slideshow-content')[target - 1]).addClass('active');
-	});
+    $('.main-slideshow-content').removeClass('active');
+    $($('.main-slideshow-content')[target - 1]).addClass('active');
+  });
 
-	setInterval(function() {
-		if(!$('.main-slideshow-nav.active').next().length) {
-			$($('.main-slideshow-nav')[0]).trigger('click');
-			return;
-		}
-		$('.main-slideshow-nav.active').next().trigger('click');
-	}, 5000);
+  setInterval(function() {
+    if(!$('.main-slideshow-nav.active').next().length) {
+      $($('.main-slideshow-nav')[0]).trigger('click');
+      return;
+    }
+    $('.main-slideshow-nav.active').next().trigger('click');
+  }, 5000);*/
+
+  // The slider being synced must be initialized first
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: true,
+    slideshowSpeed: 3000,
+    animationSpeed: 1000,
+    itemWidth: 300,
+    asNavFor: '#slider'
+  });
+ 
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    directionNav: false,
+    slideshow: true,
+    slideshowSpeed: 3000,
+    animationSpeed: 1000,
+    sync: "#carousel"
+  });
 
 })(jQuery);
